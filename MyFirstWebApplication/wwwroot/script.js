29c21010ee9f9a71505de7f7f4ba349f3133142a
Erin Qehaja <erin.qehaja@student.htldornbirn.at>
2025-03-07 14:33:26 +0100

    Css Added

--- MyFirstWebApplication/wwwroot/script.js
+++ MyFirstWebApplication/wwwroot/script.js
@@ -1,33 +1,21 @@
 ﻿async function addNumbersWithAPI() {
     const number1 = parseInt(document.getElementById("number1").value);
     const number2 = parseInt(document.getElementById("number2").value);
+    const result = document.getElementById("result");
 
     if (isNaN(number1) || isNaN(number2)) {
-        document.getElementById("result").innerText = "Bitte gültige Zahlen eingeben!";
+        result.innerText = "Ungültige Eingabe!";
         return;
     }
-
-    const data = {
-        Number1: number1,
-        Number2: number2
-    };
 
     try {
         const response = await fetch("https://localhost:7112/WeatherForecast/add", {
             method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ Number1: number1, Number2: number2 })
         });
-
-        if (!response.ok) {
-            throw new Error("Fehler bei der API-Anfrage");
-        }
-
-        const result = await response.json();
-        document.getElementById("result").innerText = result;
-    } catch (error) {
-        document.getElementById("result").innerText = "Fehler: " + error.message;
+        result.innerText = await response.json();
+    } catch {
+        result.innerText = "Fehler!";
     }
 }
